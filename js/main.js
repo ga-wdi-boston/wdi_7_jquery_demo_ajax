57ccdce6a66f99b02c204835c88aab39e8ac2c78
Max Blaushild <blaushmild@gmail.com>
2015-02-25 17:39:06 -0500

    gets all of the elements to render on page and groups each article in a div with the id of its article.id

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+  $.ajax({
+    url: "http://localhost:3000/articles",
+    type: 'GET'
+  }).done(ArticleApp.showArticles);
+
+  ;
+});
+
+var ArticleApp = ArticleApp || {};
+
+  ArticleApp.getArticles = function(data){
+    console.log(data);
+  };
+
+  ArticleApp.showArticles = function(data){
+  data.forEach(function(article){
+    $('#articles').append('<div id="article-' + article.id + '"></div>');
+    $('#article-'+ article.id).append('<h1>' + article.title + '</h1>');
+    $('#article-'+ article.id).append('<p>' + article.body + '</p>');
+    article.comments.forEach(function(comment){
+      $('#article-'+ article.id).append('<h4>' + comment.creator + '</h4>');
+      $('#article-'+ article.id).append('<p style ="font-size: 6px">' + comment.body + '</p>');
+    });
+  });
+
+
+}
+
+
+// iterate over all the articles returned from the server and create html for each article
+
+// the html for each article should be wrapped in a div that has an id "article-<id>"
+// <div id="article-17">.....</div>
