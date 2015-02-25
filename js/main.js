11f73fb9c41458b1ad0e89642548b7efe91e78de
Ross <rdegnen@gmail.com>
2015-02-25 18:48:34 -0500

    got articles to show on index.html

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+
+  $.ajax({
+    url: 'http://localhost:3000/articles',
+    type: 'GET',
+    dataType: 'json'
+  }).done(ArticleApp.getAritcles);
+
+});
+
+var ArticleApp = ArticleApp || {};
+
+ArticleApp.getAritcles = function(data) {
+  var str = JSON.stringify(data);
+  var parsed = JSON.parse(str);
+
+  parsed.forEach(function(article) {
+    $('#article-id').append('<p>' + article.id + ' ' + article.body);
+  });
+};
+
+// Iterate over all the articles return from the server
+// and create html for each article
+// the HTML for each article should be wrapped in a div with an
+// id="article-id"
