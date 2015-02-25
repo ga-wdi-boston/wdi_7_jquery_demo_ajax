cba5f4adb69fc7f792ded3ffee245331759f7eef
Annie <anne.bourgeois1@gmail.com>
2015-02-25 18:25:16 -0500

    Added comments to the articles with individualized css classes

--- js/main.js
+++ js/main.js
@@ -10,6 +10,13 @@ var ArticleApp = {};
 ArticleApp.showData = function(data){
   for(var i = 0; i < data.length; i++){
     $('#articles').append('<div id=article-' + i + '><p>' + data[i].title + '<br>' + data[i].body + '</p></div>');
+      if (data[i].comments) {
+        $('#articles').append('Comments:<ul id=comments-' + i + '>');
+        for (var j = 0; j < data[i].comments.length; j++){
+          $('#articles').append('<li id=comments-' + j + '>' + data[i].comments[j].body + '</li>');
+        };
+        $('#articles').append('</ul>');
+      };
   };
 };
 
