f1d25ee7af7f132e05f14150ed4275e96c76f821
idelairre <idelairre@gmail.com>
2015-02-26 09:24:20 -0500

    reorganizing code to put the document initializer at the top.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,24 @@
-
-var $list = $('#articles');
-
-var ArticleApp = ArticleApp || {};
-
-ArticleApp.getArticles = function(){
-    $.ajax({
-      url: 'http://localhost:3000/articles',
-      type: 'GET'
-    }).done(function(data, textStatus){
-      for (var i = 0; i < data.length; i++) {
-        var $newArticle = $('<li id="article-' + data[i].id + '">');
-        $newArticle.html('<h3>' + data[i].title + '</h3>' + ' <p>' + data[i].body + '</p>');
-        $list.append($newArticle);
-      }
-    }).fail(function(jqXHR, textStatus, errorThrown){
-      console.log(jqXHR, textStatus, errorThrown);
-    });
-};
-
 $(document).ready(function() {
-  $('#get-articles').click(ArticleApp.getArticles);
+    $('#get-articles').click(ArticleApp.getArticles);
 
-  $("input[value='Create Article']").click(function(){
-    event.preventDefault();
-  });
+    $("input[value='Create Article']").click(function() {
+        event.preventDefault();
+
+    var ArticleApp = ArticleApp || {};
+    var $list = $('#articles');
+
+    ArticleApp.getArticles = function() {
 
 
+        $.ajax({
+            url: 'http://localhost:3000/articles',
+            type: 'GET'
+        }).done(function(data) {
+            for (var i = 0; i < data.length; i++) {
+                var $newArticle = $('<li id="article-' + data[i].id + '">');
+                $newArticle.html('<h3>' + data[i].title + '</h3>' + ' <p>' + data[i].body + '</p>');
+                $list.append($newArticle);
+            }
+        });
+    };
 });
