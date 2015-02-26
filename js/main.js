44f6b02f382e95eec702a5594eccb5ae5475f01b
idelairre <idelairre@gmail.com>
2015-02-26 09:40:59 -0500

    fixing type errors commented out button action.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 $(document).ready(function() {
-    $('#get-articles').click(ArticleApp.getArticles);
+  $.ajax({
+      url: 'http://localhost:3000/articles',
+      type: 'GET'
+    }).done(ArticleApp.getArticles);
+  });
 
-    $("input[value='Create Article']").click(function() {
-        event.preventDefault();
+      // $('#get-articles').click(ArticleApp.getArticles);
+
+      // $("input[value='Create Article']").click(function() {
+      //     event.preventDefault();
+      // });
 
     var ArticleApp = ArticleApp || {};
-    var $list = $('#articles');
 
-    ArticleApp.getArticles = function() {
-
-
-        $.ajax({
-            url: 'http://localhost:3000/articles',
-            type: 'GET'
-        }).done(function(data) {
-            for (var i = 0; i < data.length; i++) {
-                var $newArticle = $('<li id="article-' + data[i].id + '">');
-                $newArticle.html('<h3>' + data[i].title + '</h3>' + ' <p>' + data[i].body + '</p>');
-                $list.append($newArticle);
-            }
-        });
-    };
+    ArticleApp.getArticles = function(data) {
+      var $list = $('#articles');
+      for (var i = 0; i < data.length; i++) {
+        var $newArticle = $('<li id="article-' + data[i].id + '">');
+        $newArticle.html('<h3>' + data[i].title + '</h3>' + ' <p>' + data[i].body + '</p>');
+        $list.append($newArticle);
+      }
+    });
+  };
 });
