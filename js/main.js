319e4e312b0e908eb6e05653020fb2757b3b0e91
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-02-26 09:37:26 -0500

    updated homework

--- js/main.js
+++ js/main.js
@@ -10,9 +10,9 @@ $(document).ready(function(){
 var ArticleApp = ArticleApp || {};
 //callback handler for all articles from server
 
- ArticleApp.getArticles = function(data){
+ ArticleApp.getArticles = function(remoteArticles){
     // debugger;
-
+    console.log(remoteArticles);
     // Iterate over all articles returned from the server
 
     // for(var i = 0; i < articles.length; i++){
@@ -20,11 +20,27 @@ var ArticleApp = ArticleApp || {};
     //   $('ul').append('<li id=article' + articles[i].id + '>' + articles[i].title);
     // }
     // $('#container').append('<ul>');
-    var articlesHTML = "<div id='" + data.id + "'><dt>Article title</dt><dd>";
-    articlesHTML += data.title + "</dd>";
-    articlesHTML +=  data.body;
-    $('#container').append(articlesHTML);
-    console.log(data);
+    // var articlesHTML = "<div id='" + data.id + "'><dt>Article title</dt><dd>";
+    // articlesHTML += data.title + "</dd>";
+    // articlesHTML +=  data.body;
+    // $('#container').append(articlesHTML);
+
+    // console.log(data[1].title);
+    var html = '<ul>';
+    remoteArticles.forEach(function(article){
+      html += "<li id='article-" article.id + "'>";
+      html += "<h3>" + article.title + "</h3">;
+      html += "<article>" + article.body + "</article>";
+      html += "</li>"
+
+      html += "<br>Comments<br>"
+      article.comments.forEach(function(comment){
+        html += "<i>" + comment.creator + "</i>";
+        html += "<p>" + comment.body + "</p>";
+      });
+    });
+    html + '</ul'>
+    $('#continer').append(html);
   };
 
 
