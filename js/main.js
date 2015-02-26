6af8244ad8750a48d57fbb662349a796a418e55d
Gina Villarreal <gina.nvillarreal@gmail.com>
2015-02-26 07:36:43 -0500

    showing some items on the page. currently says undefined.

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+  $.ajax({
+    url: 'http://localhost:3000/articles',
+    type: 'GET'
+  }).done(ArticleApp.getArticles);
+});
+
+// Create a namespace for my app
+var ArticleApp = ArticleApp || {};
+//callback handler for all articles from server
+
+ ArticleApp.getArticles = function(data){
+    // debugger;
+    console.log(data);
+    // Iterate over all articles returned from the server
+
+    // for(var i = 0; i < articles.length; i++){
+    //   //create html for each article.
+    //   $('ul').append('<li id=article' + articles[i].id + '>' + articles[i].title);
+    // }
+    // $('#container').append('<ul>');
+    var articlesHTML = "<div id='" + articles.id + "'><dt>Article</dt><dd>";
+    articlesHTML += articles.title + "</dd>";
+    articlesHTML +=  articles.body;
+    $('#container').append(articlesHTML);
+  };
+
+
+
+
+
+// The HTML for each article should we wrapped in a div that has an id "article-id" <div id="article-n">
