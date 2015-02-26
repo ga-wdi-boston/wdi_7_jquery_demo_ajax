d66d0ef91e18b015784e1907b2f6fe1f0e7322b0
katyf <katy.feng.14@gmail.com>
2015-02-26 09:08:16 -0500

    done

--- js/article_list.js
+++ js/article_list.js
@@ -4,20 +4,15 @@ Blog.ArticleList = {
   articlesCallbackHandler: function(articles){
    var articlesHTML = '',
    article;
-
-    // Build the HTML for each Article
     for(var i = 0; i < articles.length; i++){
       article = new Blog.Article(articles[i].id, articles[i].title, articles[i].body)
       articlesHTML += article.showView();
     };
-
-    // Clear list of articles.
     this.articlesListElem.empty();
-    // Fill in the article list
     this.articlesListElem.append(articlesHTML);
   },
   getArticles: function(){
-  // Retrieve all the articles
+
     $.ajax({
       url: "http://localhost:3000/articles",
       type: "GET",
@@ -27,11 +22,7 @@ Blog.ArticleList = {
   init: function(getArticlesID, articlesListID){
     this.getArticlesButton = $(getArticlesID);
     this.articlesListElem = $(articlesListID);
-
-    // Set the click handler
     this.getArticlesButton.on('click', this.getArticles.bind(this));
-
-    // Simulate a user click event. Will get all the articles
     this.getArticlesButton.trigger('click');
   }
 
