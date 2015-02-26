76391159d69e4488cfb5d52e8116829ea1a21dde
Tom Dyer <tdyer1@gmail.com>
2015-02-26 10:40:22 -0500

    Implemented a better javascript client using Object Literals and Constructors

---
+++ js/article.js
@@ -0,0 +1,33 @@
+// Create a namespace for my app
+var ArticleApp = ArticleApp || {};
+
+ArticleApp.Article = function(remoteArticle){
+  this.title = remoteArticle.title;
+  this.body = remoteArticle.body;
+  this.id = remoteArticle.id;
+
+  // Create copy of remote Comments
+  this.comments = [];
+  
+  remoteArticle.comments.forEach(function(comment){
+    var newComment = {};
+    newComment.creator = comment.creator;
+    newComment.body = comment.body;
+    this.comments.push(newComment);
+  }, this);
+};
+
+ArticleApp.Article.prototype.render = function(){
+  // spit out html for one article
+  var html = "<div id='article-" + this.id + "'>";
+  html += "<h3>" + this.title + "</h3>";
+  html += "<article>" + this.body + "</article>";
+
+  html += "<br>Comments<br>";
+  this.comments.forEach(function(comment){
+    html += "<i>" + comment.creator + "</i>";
+    html += "<p>" + comment.body + "</p>";
+  });
+  html += "</div>";
+  return html;
+};
--- js/main.js
+++ js/main.js
@@ -1,24 +1,6 @@
 $(document).ready(function(){
-  $.ajax({
-    url: 'http://localhost:3000/articles',
-    type: 'get'
-  }).done(ArticleApp.getArticles);
+  ArticleApp.ArticleList.get();
 });
 
 // Create a namespace for my app
 var ArticleApp = ArticleApp || {};
-
-// callback handler for all articles from server
-ArticleApp.getArticles = function(data){
-  debugger;
-  console.log(data);
-
-  // Iterate over all the articles returned from the
-  // server.
-
-  // And create html for each article
-
-  // The HTML for each article should be wrapped in a div
-  // that has and id "article-<id>"
-  // <div id="article-17"> .... </div>
-};
